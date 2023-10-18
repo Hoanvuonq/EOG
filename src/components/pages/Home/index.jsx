1b33169fe52629370cebe9c6baf85a54d2e0eab5
kismynamefr <kismyname.fr@gmail.com>
2023-10-18 20:19:40 +0700

    fix content

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
                     <img src={ItemEog} alt="Item EOG" className="sm:w-[400px] w-[300px]" />
                     <h1 className="text-title text-white sm:!text-[100px] !text-[80px]">$pEPE</h1>
                     <p className="text-content text-white text-center sm:px-0 px-2">CA :</p>
-                    <p className="text-content text-white text-center sm:px-0 px-2">All hail the King</p>
+                    <p className="text-content text-white text-center sm:px-0 px-2">Pepe The World Eater</p>
                     <div className="all-center gap-10 mt-10 sm:flex-row flex-col">
                         <Link to={"#"} target="_blank">
                             <button className="bg-white text-[#432627] py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">dextools chart</button>
