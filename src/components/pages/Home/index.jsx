31bd123163b127522079f555e1ae03b735a24545
hoangvuonq <hoanvuonq2002@gmail.com>
2023-10-29 22:23:34 +0700

    Update UI Pager

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Avatar from "../../../assets/avatar.jpg" 
+import Avatar from "../../../assets/item-eog-3.png" 
 import Tokenmics from "../Tokenmics";
 import Meet from "../Meet";
 import Footer from "../../layouts/Footer";
-import HalloweenBat from "../../layouts/Halloween-bat";
 
 const Home = () => {
     return (
         <div className="bg-[#23033D] w-full h-full">  
             <div className="bg-custom w-full h-full">
                 <div className="all-center w-full pt-[100px] flex-col">
-                    <HalloweenBat/>
-                    <img src={Avatar} alt="Item EOG" className="sm:w-[400px] w-[300px] rounded-full border-[6px] border-[#FDF436]" />
-                    <h1 className="text-title text-white sm:!text-[100px] !text-[80px]">$PEPIE</h1>
+                    <img src={Avatar} alt="Item EOG"  />
+                    <h1 className="text-title text-white sm:!text-[100px] !text-[80px]">$PAGER</h1>
                     <p className="text-content text-white text-center sm:px-0 px-2">CA :</p>
-                    <p className="text-content text-white text-center sm:px-0 px-2">PEPIE THE ZOMBIE</p>
+                    <p className="text-content text-white text-center sm:px-0 px-2">Meeting Pager - Elon’s smart monkey</p>
                     <div className="all-center gap-10 mt-10 lg:flex-row flex-col">
                         <Link to={"#"} target="_blank">
-                            <button className="bg-white text-[#432627] py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">dextools chart</button>
+                            <button className="bg-white py-5 px-10 rounded-xl w-[362px] h-[57px] shadow-custom text-btn all-center">dextools chart</button>
                         </Link>
-                        <Link to={"https://t.me/PepeTheWorldEatter"} target="_blank">
-                            <button className="bg-white text-[#432627] py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">telegram</button>
+                        <Link to={"#"} target="_blank">
+                            <button className="bg-white py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">telegram</button>
                         </Link>
-                        <Link to={"https://twitter.com/PepeWorldEatter"} target="_blank">
-                            <button className="bg-white text-[#432627] py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">twitter</button>
+                        <Link to={"#"} target="_blank">
+                            <button className="bg-white py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">twitter</button>
                         </Link>
                     </div>
                     <Meet/>
--- src/components/pages/Meet/index.jsx
+++ src/components/pages/Meet/index.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 const Meet = () => {
     return (
-        <div className="rounded-[40px] border-8 border-white shadow-box mt-32 w-[80%]">
+        <div className="rounded-[40px] border-8 border-white shadow-box blur-10 mt-32 w-[80%]">
             <div className="flex-col all-center sm:p-20 p-8">
-                <h1 className="text-title-box text-white text-center">MEET $PEPIE</h1>
+                <h1 className="text-title-box text-white text-center">MEET $PAGER</h1>
                 <p className="text-content-box text-white pt-10">
-                    Pepie, also known as Pepe the Zombie, is a unique and enigmatic character in the digital world. 
-                    Unlike the typical Pepe meme, Pepie stands out with his eerie appearance. 
-                    He is portrayed as a zombie-like creature with pallid, decaying green skin and vacant, haunting eyes. 
-                    Pepie's presence evokes a sense of unease and curiosity, making him a distinct and memorable figure in the meme universe
+                    The macaque monkey, named Pager, was first taught to play the video game with a joystick, and was rewarded with a fruit smoothie.
                 </p>
             </div>
         </div>
