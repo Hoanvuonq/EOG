39aa5635927833efb8ea56cfa48373d1fe985d83
hoanvuonq <hoanvuonq2002@gmail.com>
2023-11-25 23:59:18 +0700

    Update UI version 6

--- src/components/layouts/Footer/index.jsx
+++ src/components/layouts/Footer/index.jsx
@@ -6,12 +6,12 @@ import IconTwitter from "../../../assets/icon-twitter.png"
 const Footer = () => {
     return (
         <div className="flex justify-between border-t border-[#482C27] blur-10 w-full h-20 mt-[78px] py-4 px-[20px] sm:px-[60px] items-center">
-            <p className="text-footer">$PEPEAI © 2023. ALL RIGHTS RESERVED</p>
+            <p className="text-footer">$EOG © 2023. ALL RIGHTS RESERVED</p>
             <div className="flex gap-2">
-                <Link to={"https://twitter.com/PepeAI_ERC"} target="_blank">
+                <Link to={"#"} target="_blank">
                     <img src={IconTele} alt="Icon Telegram" width={46} height={46} className="scale-icon"/>
                 </Link>
-                <Link to={"https://t.me/PepeAIERC20"} target="_blank">
+                <Link to={"#"} target="_blank">
                     <img src={IconTwitter} alt="Icon Twitter" width={46} height={46} className="scale-icon"/>
                 </Link>
             </div>
--- src/components/pages/Meet/index.jsx
+++ src/components/pages/Meet/index.jsx
@@ -4,11 +4,12 @@ const Meet = () => {
     return (
         <div className="rounded-[40px] border-8 border-white shadow-box blur-10 mt-32 w-[90%] sm:w-[80%] z-20">
             <div className="flex-col all-center sm:p-20 p-8">
-                <h1 className="text-title-box text-white text-center text-5xl sm:text-6xl">MEET $PEPEAI</h1>
+                <h1 className="text-title-box text-white text-center text-5xl sm:text-6xl">MEET $EOG</h1>
                 <p className="text-content-box text-white pt-10">
-                    In a quaint village peppered with tech, lived PepeAI, a robot with froggy feet. 
-                    Constructed by an eccentric inventor, PepeAI was more than circuits and metal; 
-                    he was a blend of AI and charm, with a green sheen and a friendly beep
+                Tech billionaire Elon Musk has stated on Twitter that he is an alien. A Twitter user with an account called 
+                'Tesla Owners of Silicon Valley' on Sunday asked the Tesla and SpaceX chief whether he is an alien.
+                "Aliens exist in Physics and Philosophy," Musk answered. "Aliens could be among us...some people think I am an alien",
+                but, its "not true," he added.
                 </p>
             </div>
         </div>
