a787f15c6fa40f5cd4815a498b41407bd7e3c757
hoanvuonq <hoanvuonq2002@gmail.com>
2023-11-29 15:26:51 +0700

    Update UI version 7

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Avatar from "../../../assets/avatar-version-6.png" 
+import Avatar from "../../../assets/avatar-version-7.png" 
 import Tokenmics from "../Tokenmics";
 import Meet from "../Meet";
 import Footer from "../../layouts/Footer";
 
 const Home = () => {
     return (
-        <div className="bg-custom w-full h-full relative z-10"> 
-            <div className="bg-[#01010178] w-full h-full relative z-30">
+        <div className="bg-[#E3B425] w-full h-full relative z-10"> 
+            <div className="bg-custom w-full h-full relative z-30">
                 <div className="all-center w-full pt-[100px] flex-col z-20">
                     <img src={Avatar} alt="Item EOG" className="sm:w-[400px] w-[300px] sm:h-[400px] h-[300px]" />
                     <h1 className="text-title text-white sm:!text-[100px] !text-[60px]">$EOG</h1>
