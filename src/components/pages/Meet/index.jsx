9ceecd92275145ce80d10c61042c7012da87031b
kismynamefr <kismyname.fr@gmail.com>
2023-12-02 01:22:51 +0700

    push new build

--- src/components/pages/Meet/index.jsx
+++ src/components/pages/Meet/index.jsx
@@ -6,14 +6,11 @@ const Meet = () => {
             <div className="flex-col m-auto md:max-w-full max-w-[80%]">
                 <h1 className="text-title-box text-white md:text-start text-center md:text-5xl text-6xl">ABOUT $KABOSU</h1>
                 <p className="text-content-box md:!text-start !text-justify  md:text-black text-white md:-mt-3 mt-4">
-                    Kabosu is a female Shiba Inu dog from Japan who became famous online as the face of "Doge," 
-                    an internet meme that gained widespread popularity. The meme typically features photos of 
-                    Kabosu with multicolored text in Comic Sans font surrounding her face. The text usually 
-                    consists of short, broken English phrases that humorously represent her thoughts. Kabosu's 
-                    charming expressions and the quirky style of the meme contributed significantly to its appeal, 
-                    turning her into an iconic figure in internet culture. Kabosu was adopted by her owner, Atsuko 
-                    Sato, a Japanese kindergarten teacher, who shared her photos on a personal blog, from where they 
-                    eventually went viral and became the Doge meme.
+                    Originally formed as a "joke", Dogecoin was created by IBM software engineer Billy Markus and Adobe software engineer Jackson Palmer. 
+                    They wanted to create a peer-to-peer digital currency that could reach a broader demographic than Bitcoin. 
+                    In addition, they wanted to distance it from the controversial history of other coins. 
+                    Dogecoin was officially launched on December 6, 2013, and within the first 30 days, there were over a million visitors to Dogecoin.com.
+                    In addition, they wanted to distance it from the controversial history of other coins. Dogecoin was officially launched on December 6, 2013, and within the first 30 days, there were over a million visitors to Dogecoin.com.
                 </p>
             </div>
         </div>
