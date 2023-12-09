724630188c0482425558e0bfce78f439ee646bda
hoanvuonq <hoanvuonq2002@gmail.com>
2023-12-09 15:25:13 +0700

    Update cm Gitbook

--- src/components/layouts/Footer/index.jsx
+++ src/components/layouts/Footer/index.jsx
@@ -6,9 +6,10 @@ import IconTwitter from "../../../assets/icon-twitter.png"
 const Footer = () => {
     return (
         <div className="flex justify-between w-full py-4 px-[20px] sm:px-[60px] items-center absolute bottom-0 z-30">
-            <Link to={"#"} target="_blank" className="shadow-custom">
+            <p></p>
+            {/* <Link to={"#"} target="_blank" className="shadow-custom">
                 <p className="text-gitbook bg-white rounded-xl py-3 px-8">gitbook</p>
-            </Link>
+            </Link> */}
             <div className="flex gap-2">
                 <Link to={"#"} target="_blank">
                     <img src={IconTele} alt="Icon Telegram" width={46} height={46} className="scale-icon" />
