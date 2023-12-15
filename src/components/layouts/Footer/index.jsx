e7ed7740b1fb89d0c127d51dc03c0c1dc928d7f2
hoanvuonq <hoanvuonq2002@gmail.com>
2023-12-15 20:22:17 +0700

    update animation icon fo

--- src/components/layouts/Footer/index.jsx
+++ src/components/layouts/Footer/index.jsx
@@ -8,11 +8,11 @@ const Footer = () => {
         <div className="flex justify-between w-full py-4 px-[20px] sm:px-[60px] !pr-4 items-center absolute bottom-0 z-[70]">
             <p className="text-footer"></p>
             <div className="flex gap-2">
-                <Link to={"#"} target="_blank" className="border-2 flex gap-2 blur-1 items-center border-white py-2 px-4 rounded-2xl">
+                <Link to={"#"} target="_blank" className="border-2 shadow-custom flex gap-2 blur-1 items-center border-white py-2 px-4 rounded-2xl">
                     <img src={Telegram} alt="Icon Telegram" width={20} height={20} className="scale-icon" />
                     <p className="text-footer-box">TELEGRAM</p>
                 </Link>
-                <Link to={"#"} target="_blank" className="border-2 flex gap-2 blur-1 items-center border-white py-2 px-4 rounded-2xl">
+                <Link to={"#"} target="_blank" className="border-2 shadow-custom flex gap-2 blur-1 items-center border-white py-2 px-4 rounded-2xl">
                     <img src={Twitter} alt="Icon Twitter" width={20} height={20} className="scale-icon" />
                     <p className="text-footer-box">TWITTER</p>
                 </Link>
