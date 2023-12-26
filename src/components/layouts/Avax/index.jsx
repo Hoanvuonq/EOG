115270c57b006417820cdcd7ac3152fbd1a9b061
hoanvuonq <hoanvuonq2002@gmail.com>
2023-12-27 02:01:13 +0700

    Build Interface ZEUS

--- src/components/layouts/Avax/index.jsx
+++ src/components/layouts/Avax/index.jsx
@@ -1,34 +1,148 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import Telegram from '../../../assets/avax/Telegram.png'
-import Twitter from '../../../assets/avax/Twitter.png'
-import Discord from '../../../assets/avax/Discord.png'
-import LOONG from '../../../assets/avax/avax_1.png'
+import IconCoppy from '../../../assets/copy.png'
+import ZEUS from '../../../assets/avax/ZEUS.png'
+import ClipboardJS from 'clipboard';
+
+const splitAddress = (address, options) => {
+    const isVersion2 = options?.isVersion2 || false;
+    const numSplit = options?.numSplit || 10;
+
+    if (address) {
+        return (
+            <>
+                {address.substring(0, isVersion2 ? 4 : numSplit)}
+                {isVersion2 ? ' **** **** ' : ' ... '}
+                {address.substring(address.length - (isVersion2 ? 4 : numSplit))}
+            </>
+        );
+    }
+
+    return '';
+};
 
 
 
 const Avax = () => {
-  return (
-    <div className='md:block hidden relative z-[120]'>
-      <div className="absolute w-full z-[100]">
-        <div className="absolute z-[100]">
-          <div className="flex flex-col justify-between p-5 w-full h-screen">
-            <img src={LOONG} alt="Design" width={18} />
-            <div className="flex flex-col items-center gap-4">
-              <Link to={"https://t.me/LoongAvax"} target="_blank">
-                <button className="!text-white uppercase shadow-custom text-btn all-center">
-                  <img src={Telegram} alt="Design" width={12} /></button>
-              </Link>
-              <Link to={"https://twitter.com/LoongAvax"} target="_blank">
-                <button className="!text-white uppercase shadow-custom text-btn all-center">
-                  <img src={Twitter} alt="Design" width={12} /></button>
-              </Link>
+    const contractAddress = '';
+    const buttonRef = useRef(null);
+
+    useEffect(() => {
+        const clipboard = new ClipboardJS(buttonRef.current);
+
+        clipboard.on('success', (e) => {
+            console.log('Copied to clipboard:', e.text);
+        });
+
+        return () => {
+            clipboard.destroy();
+        };
+    }, []);
+    return (
+        <div className='md:block w-full hidden relative z-[120]'>
+            {/* <img src={ZEUS} alt="ZEUS" className='absolute right-0 bottom-[500px]' /> */}
+            <div className="absolute w-full first-lette left-[8%] bottom-14 z-[100]">
+                <div className="md:!flex block justify-between items-center w-full relative z-50 md:px-20 px-0">
+                    <div className="mt-10">
+                        <div className="all-center flex-col md:pt-0 pt-32  md:pb-0 pb-10 relative z-40">
+                            <h1 className="text-title-box text-white md:text-start text-center md:text-[80px] text-[52px] md:hidden block md:absolute relative z-[70] md:mt-0 -mt-20">Tokenomic</h1>
+                            <div className="all-center md:!flex !hidden md:!items-start !items-center md:gap-2 gap-4 md:flex-row flex-col md:-mt-3 mt-10">
+                                <div className="flex md:gap-0 gap-4 md:flex-row flex-col">
+                                    <div className="flex flex-col">
+                                        <Link to={'/'}>
+                                            <button className="text-token flex gap-2 text-white py-5 text-center px-4 md:w-[410px] w-[380px] md:h-10 h-20 shadow-custom all-center md:rounded-none rounded-xl ">
+                                                <p className='font-bold text-[#1257FA] text-border-1 !text-2xl'>Total Supply</p>
+                                                <p> - </p>
+                                                <p className='font-bold !text-3xl'> 100,000,000 $ZEUS</p>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <Link to={'/'}>
+                                            <button className="text-token flex gap-2 text-white py-5 text-center px-4 md:w-[200px] w-[380px] md:h-10 h-20 shadow-custom all-center md:rounded-none rounded-xl ">
+                                                <p className='font-bold text-[#1257FA] text-border-1 !text-2xl'>TAX</p>
+                                                <p> - </p>
+                                                <p className='font-bold !text-3xl'> 0% / 0% </p>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                                <div className="flex md:gap-10 gap-4 md:flex-row flex-col">
+                                    <div className="flex flex-col">
+                                        <Link to={'/'}>
+                                            <button className="text-token flex gap-2 text-white py-5 text-center px-4 md:w-[250px] w-[380px] md:h-10 h-20 shadow-custom all-center md:rounded-none rounded-xl ">
+                                                <p className='font-bold text-[#1257FA] text-border-1 !text-2xl'>LP LOCKED</p>
+                                                <p> - </p>
+                                                <p className='font-bold !text-3xl'> 9 MONTHS </p>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <Link to={'/'}>
+                                            <button className="text-token flex gap-2 text-white py-5 text-center px-4 md:w-[250px] w-[380px] md:h-10 h-20 shadow-custom all-center md:rounded-none rounded-xl ">
+                                                <p className='font-bold text-[#1257FA] text-border-1 !text-2xl'>OWNERSHIP</p>
+                                                <p> - </p>
+                                                <p className='font-bold !text-3xl'> RENOUNCED </p>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="all-center md:!hidden !flex md:!items-start !items-center md:gap-0 gap-4 md:flex-row flex-col md:-mt-3 mt-10">
+                                <div className="flex md:gap-0 gap-4 md:flex-row flex-col">
+                                    <div className="flex flex-col">
+                                        <Link to={'/'}>
+                                            <button className="text-token flex gap-2 rounded-full border-2 border-white text-white py-5 text-center px-4 md:w-[380px] w-[380px] md:h-10 h-20 shadow-custom all-center md:rounded-none blur-10 ">
+                                                <p className='font-bold'>Total Supply</p>
+                                                <p> - </p>
+                                                <p className='font-bold'> 999,999,999,999 $LOONG</p>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <Link to={'/'}>
+                                            <button className="text-token flex gap-2 rounded-full border-2 border-white text-white py-5 text-center px-4 md:w-[200px] w-[380px] md:h-10 h-20 shadow-custom all-center md:rounded-none blur-10 ">
+                                                <p className='font-bold'>TAX</p>
+                                                <p> - </p>
+                                                <p className='font-bold'> 0% / 0% </p>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                                <div className="flex md:gap-0 gap-4 md:flex-row flex-col">
+                                    <div className="flex flex-col">
+                                        <Link to={'/'}>
+                                            <button className="text-token flex gap-2 rounded-full border-2 border-white text-white py-5 text-center px-4 md:w-[240px] w-[380px] md:h-10 h-20 shadow-custom all-center md:rounded-none blur-10 ">
+                                                <p className='font-bold'>LP LOCKED</p>
+                                                <p> - </p>
+                                                <p className='font-bold'> 9 MONTHS </p>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <Link to={'/'}>
+                                            <button className="text-token !font-bold rounded-full border-2 border-white text-white py-5 text-center px-4 md:w-[260px] w-[380px] md:h-10 h-20 shadow-custom all-center md:rounded-none blur-10 ">
+                                                <p className='font-bold!'>OWNERSHIP RENOUNCED</p>
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="gap-2 cursor-pointer all-center md:mt-3 mt-12 ">
+                                <p className="text-address items-center flex gap-2 text-white pb-1  ">
+                                    Contract Address -  <p ref={buttonRef} data-clipboard-text={contractAddress}>
+                                        {splitAddress(contractAddress, { numSplit: 10 })}
+
+                                    </p>
+                                    <img src={IconCoppy} width={20} height={20} alt="Icon Coppy" className="ml-2 shadow-custom" />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Avax;
--- src/components/pages/Meet/index.jsx
+++ src/components/pages/Meet/index.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Title from '../../../assets/Title.png'
+import BuyNow from '../../../assets/buy-now.png'
+import Telegram from '../../../assets/telegram.png'
+import Twitter from '../../../assets/twitter.png'
 
 const Meet = () => {
     return (
-        <div className="all-center md:!items-start flex-col w-full  md:pl-20 pl-0">
-            <div className="flex-col m-auto max-w-full all-center">
-                <h1 className="text-title-box text-white md:text-start text-center md:text-[100px] text-[52px] md:mt-0 mt-10">Ready for <span className='md:text-[120px] text-[70px]'>$LOONG</span></h1>
-                <p className="text-content-box text-justify text-white md:mt-6 mt-4 relative md:px-0 px-2">
+        <div className="all-center md:!items-start md:mt-0 mt-10 gap-10 flex-col w-full ">
+            <div className="w-full flex-col gap-4 all-center !justify-start">
+                {/* <h1 className="text-title-box text-white md:text-start text-center md:text-[100px] text-[52px] md:mt-0 mt-10">Ready for <span className='md:text-[120px] text-[70px]'>$LOONG</span></h1> */}
+                <img src={Title} alt="Title" width={640} className='md:w-[640px] w-[360px]' />
+                {/* <p className="text-content-box text-justify text-white md:mt-6 mt-4 relative md:px-0 px-2">
                     In Chinese culture, the dragon represents good luck, strength, health, and also the male element Yang. The dragon is unique because it is the only mythical creature of all the animals in the Chinese zodiac and babies are born in the year of the dragon more than any other animal.
-                </p>
-            </div>
-            <div className="rounded-full shadow-custom bg-white mt-14 text-black w-[160px] h-[46px] py-3 px-8 all-center unbounded font-bold">
-                <div className="">
-                    <Link to={'https://traderjoexyz.com/avalanche/trade?outputCurrency=0x5A2C298902f5ba4C3d8B88f9F7cf8f5be72303B9'}>Buy Now</Link>
+                </p> */}
+                <div className="flex gap-4 md:flex-row flex-col">
+                    <Link to={'#'}>
+                        <img src={Telegram} alt="Buy Now" width={120} height={30} className='md:w-[120px] w-[380px] md:h-12 h-24 shadow-custom md:block hidden' />
+                    </Link>
+                    <Link to={'#'}>
+                        <img src={Twitter} alt="Buy Now" width={120} height={30} className='md:w-[120px] w-[380px] md:h-12 h-24 shadow-custom md:block hidden' />
+                    </Link>
+                    <Link to={"https://t.me/WojakSolanaOfficial"} target="_blank">
+                        <button className="py-5 px-10 md:w-[280px] w-[360px] md:h-9 h-20 rounded-full border-2 border-white blur-10 uppercase shadow-custom text-btn all-center md:!hidden !flex">telegram</button>
+                    </Link>
+                    <Link to={"https://twitter.com/Wojak_Solana"} target="_blank">
+                        <button className="py-5 px-10 md:w-[280px] w-[360px] md:h-9 h-20 rounded-full border-2 border-white blur-10 uppercase shadow-custom text-btn all-center md:!hidden !flex">twitter</button>
+                    </Link>
                 </div>
             </div>
+            <button className="shadow-custom w-full all-center md:!justify-end !justify-center">
+                <Link to={''}>
+                    <img src={BuyNow} alt="Buy Now" width={300} className='md:w-[300px] w-[360px]' />
+                </Link>
+            </button>
         </div>
     );
 };
